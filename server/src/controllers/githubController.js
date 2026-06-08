2639da73faa5f66add4ebafc30022ac783b538e3
Shivamjoshi902 <sj8630470355@gmail.com>
2026-06-08 19:46:11 +0530

    Improve API error handling

--- server/src/controllers/githubController.js
+++ server/src/controllers/githubController.js
@@ -1,25 +1,34 @@
 import {
-  fetchUserProfile,
-  fetchUserRepositories,
+    fetchUserProfile,
+    fetchUserRepositories,
 } from "../services/githubService.js";
 
 export const getUserProfile = async (req, res) => {
-  try {
-    const { username } = req.params;
+    try {
+        const { username } = req.params;
 
-    const user = await fetchUserProfile(username);
+        const user = await fetchUserProfile(username);
 
-    const repos = await fetchUserRepositories(username);
+        const repos = await fetchUserRepositories(username);
 
-    res.json({
-    success: true,
-    user,
-    repos,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
+        res.json({
+        success: true,
+        user,
+        repos,
+        });
+    }
+    catch (error) {
+
+        if (error.response?.status === 404) {
+            return res.status(404).json({
+                success: false,
+                message: "GitHub user not found",
+            });
+        }
+
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+        });
+    }
 };
